1caa91deaa7c818779b21e8561d436cdcfd6cbab
Jioh_Park <jp66585@my.utexas.edu>
2025-11-05 16:49:16 -0600

    f2025c practice

--- reorder.c
+++ reorder.c
@@ -42,12 +42,16 @@ static struct listnode* merge(struct listnode* node1, struct listnode* node2) {
     struct listnode head = {0};
     struct listnode* itr = &head;
     while (node1 || node2) {
-        itr->next = node1;
-        node1 = node1->next;
-        itr = itr->next;
-        itr->next = node2;
-        itr = itr->next;
-        node2 = node2->next;
+        if(node1){
+            itr->next = node1;
+            node1 = node1->next;
+            itr = itr->next;
+        }
+        if(node2){
+            itr->next = node2;
+            node2 = node2->next;
+            itr = itr->next;
+        }
     }
     if (node1) {
         itr->next = node1;
@@ -73,7 +77,7 @@ static void free_node(struct listnode** node) {
 
 void free_list(struct listnode* node) {
     while (node) {
+        node = node->next;
         free_node(&node);
-	node = node->next;
     }
 }
